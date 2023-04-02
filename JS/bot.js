0c9cc1b80a20c17250fb9546e66301277a91bc58
maksimmartynov <129686329+maksimmartynov@users.noreply.github.com>
2023-04-02 21:08:38 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -5,8 +5,8 @@ console.log('Starting...')
 function createBot () {
     const bot = mineflayer.createBot({
     host: "localhost",
-    port: "25565",
-    username: "Test",
+    port: "23065",
+    username: "Aternos bot",
     version: false
     })
     bot.on('login', function() {
